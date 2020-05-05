3c940178c2fac3a8d095ea8c5fe61319ad696b06
Rachel Higley <rhigley@fullsail.com>
2020-05-05 08:29:48 -0400

    Starting build of content data

--- src/components/Sections/SectionContentOne.js
+++ src/components/Sections/SectionContentOne.js
@@ -4,10 +4,10 @@ import styled from '@emotion/styled';
 const Container = styled.section`
   position: relative;
 
-  width: 100%;
-  height: 40vh;
+  max-width: 1170px;
+  margin: 0 auto;
 `;
-const Wrapper = styled.div`
+const Content = styled.article`
   grid-column-gap: 30px;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
   max-width: 1440px;
@@ -15,12 +15,26 @@ const Wrapper = styled.div`
   padding: 30px 0;
 `;
 
+const Title = styled.h1`
+display: flex;
+align-items: center;
+
+color: #469FD1;
+font-weight: 800;
+font-size: 30px;
+font-family: Biryani;
+font-style: normal;
+line-height: 53px;
+`;
+
 
 export default ({ section }) => {
   console.log('section content-one', section);
   return (
     <Container>
-      <Wrapper />
+      <Content>
+        <Title>{section.title}</Title>
+      </Content>
     </Container>
   );
 };
