4193da63a3815aac38b4e6fead6ec5ec5f90640a
Matt <mattpezzente@gmail.com>
2020-07-30 15:50:55 -0400

    changed build target

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,9 @@
-const React = require('react');
-const ContextSettingsProvider = require('./src/contexts/context-settings');
+// const React = require('react');
+// const ContextSettingsProvider = require('./src/contexts/context-settings');
 
-// eslint-disable-next-line import/prefer-default-export
-export const wrapRootElement = ({ element }) => (
-  <ContextSettingsProvider>
-    {element}
-  </ContextSettingsProvider>
-);
+// // eslint-disable-next-line import/prefer-default-export
+// export const wrapRootElement = ({ element }) => (
+//   <ContextSettingsProvider>
+//     {element}
+//   </ContextSettingsProvider>
+// );
