6b5101fd0c19642b7c7b927bb773700560e786b1
Matt <mattpezzente@gmail.com>
2020-09-02 15:33:07 -0400

    mobile fixes to images

--- src/components/Sections/SectionContentImages.js
+++ src/components/Sections/SectionContentImages.js
@@ -13,14 +13,16 @@ const Grid = styled.div`
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
   max-width: 1440px;
   margin: 0 auto;
+  padding: 0 30px 30px 30px;
+
   @media(max-width: ${mq.xs}px) {
-    grid-gap: 10px 0;
+    grid-gap: 15px;
+    padding: 0 30px 15px 30px;
   }
 `;
 
 const Image = styled.img`
   width: 100%;
-  padding: 0px 30px 30px 30px;
 `;
 
 export default ({ section }) => (
@@ -31,11 +33,11 @@ export default ({ section }) => (
           css={css`
             grid-column: 3 / span ${8 / section.list.length};
 
-            @media(max-width: ${mq.sm}px) {
+            @media(max-width: ${mq.md}px) {
               grid-column: 2 / span ${10 / section.list.length};
             }
             @media(max-width: ${mq.xs}px) {
-              grid-column: 1 / span ${12 / section.list.length};
+              grid-column: span ${12 / section.list.length};
             }
         `}
           src={image.image}
@@ -47,11 +49,13 @@ export default ({ section }) => (
           css={css`
           grid-column: ${3 + (i * 4)} / span 4;
 
-          @media(max-width: ${mq.sm}px) {
-            grid-column: ${3 + (i * 4)}  / span ${10 / section.list.length}
+          @media(max-width: ${mq.md}px) {
+            grid-column: ${2 + (i * 5)}  / span ${10 / section.list.length};
           }
+
           @media(max-width: ${mq.xs}px) {
-            grid-column: 1  / span ${12 / section.list.length}
+            grid-column: span ${12 / section.list.length};
+            padding: 0;
           }
           `}
           src={image.image}
