7766eb6e026336e54f778d4ff78738094e06ced4
Matt <mattpezzente@gmail.com>
2021-09-03 16:29:50 -0400

    fixed

--- src/templates/PressTemplate.js
+++ src/templates/PressTemplate.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
 
 // Components
+import { Helmet } from 'react-helmet';
 import Layout from '../components/Layouts/Layout';
 import SectionBanner from '../components/Sections/SectionBanner';
 import {
